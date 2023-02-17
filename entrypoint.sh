791962b4ce58eda935f0ae8c4855fad3cbfda495
Piotr Sarnacki <drogus@gmail.com>
2023-02-17 13:09:26 +0100

    Bring back TOKEN usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-if [ -z "${RUNNER_TOKEN:-}" ]
+if [ -z "${TOKEN:-}" ]
 then
-  echo "RUNNER_TOKEN is required"
+  echo "TOKEN is required"
   exit 1
 fi
 
 if [ -n "${ORG:-}" ]
 then
+  API_PATH=orgs/${ORG}
   CONFIG_PATH=${ORG}
 elif [ -n "${OWNER:-}" ] && [ -n "${REPO:-}" ]
 then
+  API_PATH=repos/${OWNER}/${REPO}
   CONFIG_PATH=${OWNER}/${REPO}
 else
   echo "[ORG] or [OWNER and REPO] is required"
   exit 1
 fi
+
+RUNNER_TOKEN=$(curl -s -X POST -H "authorization: token ${TOKEN}" "https://api.github.com/${API_PATH}/actions/runners/registration-token" | jq -r .token)
 
 cleanup() {
   ./config.sh remove --token "${RUNNER_TOKEN}"
